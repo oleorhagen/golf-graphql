6875aa948d931e1a9cb2a5f15aca86a0cc3158c0
Ole P. Orhagen <ole@orhagen.no>
2025-04-22 19:31:03 +0200

    chore: Add a go generate comment for gqlgen
    So that now I can run go generate ./...

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,11 +1,13 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen generate
+
 import "github.com/oleorhagen/golf-graphql/graph/model"
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{
+type Resolver struct {
 	todos []*model.Todo
 }
