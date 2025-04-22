d31d73e19e373773d5b51d392341fe77c14b0e6a
Ole P. Orhagen <ole@orhagen.no>
2025-04-22 19:04:46 +0200

    chore: Clean up the source a little
    Also implement the todo resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,17 +8,26 @@ import (
 	"context"
 	"fmt"
 
+	"math/rand"
+
 	"github.com/oleorhagen/golf-graphql/graph/model"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	randNumber := rand.Int()
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", randNumber),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return r.todos, nil
 }
 
 // Mutation returns MutationResolver implementation.
