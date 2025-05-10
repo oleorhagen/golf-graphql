c141de9e53b9d38a8f67767e89be70f3c851ed51
Ole P. Orhagen <ole@orhagen.no>
2025-05-10 12:39:45 +0200

    build: Add a Dockerfile for running the server in

--- server.go
+++ server.go
@@ -34,13 +34,6 @@ func main() {
 	}
 	defer conn.Close()
 
-	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-	// 	os.Exit(1)
-	// }
-	// defer conn.Close(context.Background())
-
 	var greeting string
 	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
 	if err != nil {
@@ -50,7 +43,7 @@ func main() {
 
 	fmt.Println(greeting)
 
-	port := os.Getenv("PORT")
+	port := os.Getenv("DATABASE_PORT")
 	if port == "" {
 		port = defaultPort
 	}
